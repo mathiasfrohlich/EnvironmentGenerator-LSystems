ccfdc027e290fdf4b56703636d435a3efcb70967
Mathias Frohlich <mathias.frohlich@gmail.com>
2015-12-08 12:54:48 +0100

    Scene

--- Assets/EnviromentGenerator/EntityCreators/GrassCreator.cs
+++ Assets/EnviromentGenerator/EntityCreators/GrassCreator.cs
@@ -34,22 +34,7 @@ public class GrassCreator : EntityCreator{
 			grassEntity.name = "GrassEntity";
 			grassEntity.transform.parent = parrent.transform;
 		}
-
-
-//		Debug.Log("Create Grass");
-//		GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Cube);
-//		capsule.transform.position = position;
-//
-//
-//		Terrain t = gameObject.GetComponent<Terrain>();
-//
-//		float threshold = 0.5f;
-//
-//		DetailMapCutoff(t,threshold);
-
-
-//		gameObject.GetComponent<Terrain>().terrainData.SetDetailLayer(position.x,position.y,LayerMask.GetMask("Default"))
-
+		
 		return parrent;
 	}
 
@@ -61,27 +46,5 @@ public class GrassCreator : EntityCreator{
 	public override void tearDown ()
 	{
 
-	}
-
-	// Set all pixels in a detail map below a certain threshold to zero.
-	public void DetailMapCutoff(Terrain t, float threshold) {
-//		// Get all of layer zero.
-//		var map = t.terrainData.GetDetailLayer(0, 0, t.terrainData.detailWidth, t.terrainData.detailHeight, 0);
-//		
-//		// For each pixel in the detail map...
-//		for (var y = 0; y < t.terrainData.detailHeight; y++) {
-//			for (var x = 0; x < t.terrainData.detailWidth; x++) {
-//				// If the pixel value is below the threshold then
-//				// set it to zero.
-//				Debug.Log(map[x,y]);
-//
-//				if (map[x, y] < threshold) {
-//					map[x, y] = 0;
-//				}
-//			}
-//		}
-//		
-//		// Assign the modified map back.
-//		t.terrainData.SetDetailLayer(0, 0, 0, map);
 	}
 }
---
+++ Assets/EnviromentGenerator/MapFillers/WeightedMapFiller/WeightedMapFiller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeightedMapFiller : MonoBehaviour {
+
+	[Range(0, 100)]
+	public int nothingWeigth;
+	[Range(0, 100)]
+	public int greenWeigth;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
